876d39f4987b24e71d9855dd884df54fbaf68450
salonitiwari24 <saloni.tiwari24@vit.edu>
2026-08-25 15:21:54 +0530

    Fixing workflow

---
+++ app.py
@@ -0,0 +1,14 @@
+def predict_result(marks):
+    if marks >= 40:
+        return "PASS"
+    else:
+        return "FAIL"
+
+
+if __name__ == "__main__":
+    marks = 75
+    marks = int(input("Enter student marks: "))
+    result = predict_result(marks)
+
+    print("Student Marks:", marks)
+    print("Predicted Result:", result)
---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import predict_result
+
+def test_pass_student():
+    assert predict_result(75) == "PASS"
+
+def test_fail_student():
+    assert predict_result(30) == "FAIL"
+
+def test_boundary():
+    assert predict_result(40) == "PASS"
